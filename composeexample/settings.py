adcc1394e7fddddfcc20e1d6b9b89c233fa00364
Guinslym <gmondesi@pcsun.ad.uottawa.ca>
2017-04-08 18:54:21 -0400

    add db postgres

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'composeexample.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'postgres',
+         'USER': 'postgres',
+         'HOST': 'db',
+         'PORT': 5432,
+     }
+ }
 
 
 # Password validation
